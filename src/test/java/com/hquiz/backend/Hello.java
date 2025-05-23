2da755f6a9be922713622f4a996ce0ca4ba49e94
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-05-23 21:38:45 +0530

    Added RESTController

--- src/test/java/com/hquiz/backend/Hello.java
+++ src/test/java/com/hquiz/backend/Hello.java
@@ -1,7 +1,9 @@
 package com.hquiz.backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Hello {
    @GetMapping("/hello")
     public String sayHello() {
