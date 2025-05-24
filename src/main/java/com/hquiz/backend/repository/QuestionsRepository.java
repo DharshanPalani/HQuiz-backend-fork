21618a40bc69344e87532e5491f19937de327bfd
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-05-24 13:54:47 +0530

    Removed shi in questionsRepository

--- src/main/java/com/hquiz/backend/repository/QuestionsRepository.java
+++ src/main/java/com/hquiz/backend/repository/QuestionsRepository.java
@@ -1,6 +1,5 @@
 package com.hquiz.backend.repository;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +9,5 @@ import com.hquiz.backend.model.Questions;
 @Repository
 // This is a way to connect to the postgress db, idk the rest
 public interface QuestionsRepository extends JpaRepository<Questions, Long>{
-    Optional<Questions> findByQuestionID(Long id);
+
 }
